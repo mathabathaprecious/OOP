e3e12b047fb213ef1e599a1638a4d81a45a0e508
mathabathaprecious <mpuruprecious21@gmail.com>
2023-01-24 12:42:51 +0200

    Add project files.

---
+++ Learners.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class Learners
+    {
+        public string firstname;
+        public string lastname;
+        public char initial;
+        public string gender;
+        public int age;
+        public Learners(string Firstname, string Lastname, char Initial,string Gender, int Age)
+        {
+            firstname = Firstname;
+            lastname = Lastname;
+            initial = Initial;
+            gender = Gender;
+            age = Age;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using OOP;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Learners learner1 = new Learners("Malesela", "Letsoalo", 'J', "Male", 30);
+        Console.WriteLine(learner1.firstname+" "+learner1.lastname+" "+learner1.initial+" "+learner1.gender+" "+learner1.age);
+    }
+}
